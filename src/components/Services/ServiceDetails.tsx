54cb0cea2a6ab21d37ddf05f23a4236fd7b4963e
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-08-27 20:47:57 +0200

    handled color and add select option to form and translated the non tranlsated

--- src/components/Services/ServiceDetails.tsx
+++ src/components/Services/ServiceDetails.tsx
@@ -1,44 +1,66 @@
-import { servicesItems } from "@/data";
-import { useParams } from "react-router-dom";
-import serviceImage from "../../assets/service.jpg";
-
-const ServiceDetails = () => {
-    const { slug } = useParams();
-
-    const services = servicesItems
-    const service = services.find((s) => s.slug === slug);
-
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import { AnimatePresence, motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
+const ServiceDetails = ({ openDialog = true, setOpenDialog, service }: { openDialog: boolean; setOpenDialog: (open: boolean) => void; service: any }) => {
+    const { t } = useTranslation();
     if (!service) return <p className="text-center mt-10">Service not found</p>;
-
     return (
         <section className="relative container mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 place-items-center h-full gap-5 max-lg:pb-5 min-h-[calc(100dvh-64px)]">
 
             {/* Decorative shapes */}
-            <div className="absolute top-10 left-10 w-32 h-32 bg-accent/20 rounded-full mix-blend-multiply animate-pulse"></div>
-            <div className="absolute - bottom-40 right-40 w-32 h-32 border-2 border-accent/20 rounded-full mix-blend-multiply animate-pulse"></div>
+            {/* <div className="absolute top-10 left-10 w-32 h-32 bg-accent/20 rounded-full mix-blend-multiply animate-pulse"></div>
+            <div className="absolute - bottom-40 right-40 w-32 h-32 border-2 border-accent/20 rounded-full mix-blend-multiply animate-pulse"></div> */}
 
             {/* Text content */}
-            <div className="relative p-8 rounded-2xl z-10">
-                <h1 className="text-xl lg:text-3xl font-bold mb-4 text-subtle">{service.title}</h1>
-                <p className="text-muted-foreground mb-6">{service.description}</p>
-                <h2 className="text-xl font-semibold mb-4 text-color-main">الخصائص :</h2>
-                <ul className="list-disc list-inside space-y-2">
-                    {service.features.map((f: string, idx: number) => (
-                        <li key={idx} className="text-card-foreground">
-                            {f}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+
 
             {/* Image */}
-            <div className="relative rounded-xl overflow-hidden z-10">
-                <img src={serviceImage} alt={service.title} className="w-full"/>
-                {/* Shape overlay on image */}
+            {/* <div className="relative rounded-xl overflow-hidden z-10">
+                <img src={serviceImage} alt={service.title} className="w-full" />
                 <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-tr from-accent/20 to-color-main/20 mix-blend-overlay pointer-events-none"></div>
-            </div>
-        </section>
+            </div> */}
+            <Dialog
+                open={openDialog}
+
+                modal
+                onOpenChange={(open) => setOpenDialog(open)}
+            >
+                <DialogContent className="overflow-hidden" >
+                    <DialogHeader >
+                        <DialogTitle>{service.title}</DialogTitle>
+                    </DialogHeader>
+                    <AnimatePresence mode="wait">
+                        <motion.div
+                            key="form"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <div className="relative p-8 rounded-2xl z-10">
+                                <h1 className="text-xl lg:text-3xl font-bold mb-4 text-subtle">{service.title}</h1>
+                                <p className="text-muted-foreground mb-6">{service.description}</p>
+                                <h2 className="text-xl font-semibold mb-4 text-color-main">{t("services.features")} :</h2>
+                                <ul className="list-disc list-inside space-y-2 ">
+                                    {service.features.map((f: string, idx: number) => (
+                                        <li key={idx} className="text-card-foreground">
+                                            {f}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </motion.div>
+                    </AnimatePresence>
+                </DialogContent>
+            </Dialog>
+        </section >
     );
 };
 
 export default ServiceDetails;
+
--- src/components/shared/ContactForm.tsx
+++ src/components/shared/ContactForm.tsx
@@ -1,55 +1,149 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { z } from "zod";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "../ui/use-toast";
 
-const ContactForm = () => {
+const contactSchema = z.object({
+  name: z.string().min(2, { message: "الاسم مطلوب على الأقل حرفين" }),
+  phone: z
+    .string()
+    .regex(/^01[0-9]{9}$/, { message: "رقم الهاتف غير صحيح" }),
+  city: z.string().min(2, { message: "المدينة مطلوبة" }),
+  option: z.string().nonempty({ message: "يجب اختيار نوع الطلب" }),
+});
+
+type ContactFormValues = z.infer<typeof contactSchema>;
+
+const ContactForm = ({ onClick ,isBooking}: { onClick?: () => void ,isBooking?:boolean}) => {
   const { t } = useTranslation();
 
-  return (
-    <>
-      <form className="space-y-4">
-        <input
-          required
-          type="text"
-          placeholder={t("contactForm.name")}
-          className="w-full border rounded-lg p-2 lg:p-3"
-        />
-        <input
-          required
-          type="tel"
-          placeholder={t("contactForm.phone")}
-          className="w-full border rounded-lg p-2 lg:p-3 placeholder:!text-end"
-        />
-        <input
-          required
-          type="email"
-          placeholder={t("contactForm.email")}
-          className="w-full border rounded-lg p-2 lg:p-3"
-        />
-      
-            <input
-              required
-              type="city"
-              placeholder={t("contactForm.city")}
-              className="w-full border rounded-lg p-2 lg:p-3"
-            />
-         
-     
-        
+  const form = useForm<ContactFormValues>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: {
+      name: "",
+      phone: "",
+      city: "",
+      option: "",
+    },
+  });
 
-        
-        <textarea
-          required
-          placeholder={t("contactForm.message")}
-          className="w-full border rounded-lg p-2 lg:p-3"
+  function onSubmit(values: ContactFormValues) {
+    console.log("Form Data:", values);
+    if (!isBooking) {
+      toast({
+        title: "تم الارسال بنجاح",
+        className: "bg-green-400/80 text-white",
+
+      })
+    }
+    if (onClick) onClick();
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 text-start  w-full lg:w-3/4 mx-auto"
+        dir="rtl"
+      >
+        {/* Name */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input className="w-full border rounded-lg p-2 lg:p-3 h-12" placeholder={t("contactForm.name")} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        <Button type="submit" className="w-full  ">
+
+        {/* Phone */}
+        <FormField
+          control={form.control}
+
+          name="phone"
+          render={({ field }) => (
+            <FormItem className="">
+              <FormControl>
+                <Input className="w-full border rounded-lg p-2 lg:p-3 h-12"
+                  placeholder={t("contactForm.phone")} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* City */}
+        <FormField
+          control={form.control}
+          name="city"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input className="w-full border rounded-lg p-2 lg:p-3 h-12" placeholder={t("contactForm.city")} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Select Option */}
+        <FormField
+          control={form.control}
+          name="option"
+          render={({ field }) => (
+            <FormItem >
+              <Select dir="rtl" onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-full border rounded-lg p-2 lg:p-3 h-12">
+                    <SelectValue placeholder={t("contactForm.selectOption")} />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="option1" className="w-full  rounded-lg p-2 lg:p-3 h-12">
+                    {t("contactForm.option1") || "استشارة قانونية"}
+                  </SelectItem>
+                  <SelectItem value="option2" className="w-full  rounded-lg p-2 lg:p-3 h-12">
+                    {t("contactForm.option2") || "حجز موعد"}
+                  </SelectItem>
+                  <SelectItem value="option3" className="w-full  rounded-lg p-2 lg:p-3 h-12">
+                    {t("contactForm.option3") || "استفسار عام"}
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full">
           {t("contactForm.send")}
         </Button>
       </form>
-    </>
+    </Form>
   );
 };
 
